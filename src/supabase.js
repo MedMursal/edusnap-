278993106bd004a575817fa43e3f0fac81142ede
Mursal <gasanov-mursal@mail.ru>
2026-03-09 02:08:14 +0300

    update supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const SUPABASE_URL = "https://jwsquzinwhjfklchkmst.supabase.co"
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3c3F1emlud2hqZmtsY2hrbXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4OTA0NDMsImV4cCI6MjA4ODQ2NjQ0M30.C8kcMd3iA_7QHgMOlXFEMLt80qwqbfy2kvTuKddhCEQ"
+const SUPABASE_URL = "https://zdaofrgfojbjozvbnzjk.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkYW9mcmdmb2piam96dmJuemprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMDg3MjEsImV4cCI6MjA4ODU4NDcyMX0.Eur7oPY17SQbUU2g98ibsRkBjMbRQedG2Q7h4IdOSmA"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
